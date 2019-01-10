f1ec0363a4554bafe6e1b06759c64efdb12a873e
SaharDivan <saharsahebdivan@gmail.com>
2019-01-10 10:45:15 +0100

    week8LevelScouter

---
+++ src/Lambdas/PowerLevelScouter.java
@@ -0,0 +1,21 @@
+package Lambdas;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public class PowerLevelScouter {
+
+    public Integer scout(String name, Function<String,Integer> powerRule){
+        List<String> letters = Arrays.asList(name.split(""));
+        Integer sum = 0;
+        for (String letter : letters) {
+            sum += powerRule.apply(letter);
+        }
+       return sum;
+    }
+
+    Function<String,Integer> power = s->(int)s.charAt(0);
+    Function<String,Integer> powerEnhanced = s->(int)s.toLowerCase().charAt(0);
+
+
+}
